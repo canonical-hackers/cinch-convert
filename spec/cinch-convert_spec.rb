cd5e00f85033b6fd00c54face62c4f4c3e6a3fab
Brian Haberer <bhaberer@gmail.com>
2013-07-09 14:41:18 -0700

    Fixing tests to work with cinch-test v0.3

--- spec/cinch-convert_spec.rb
+++ spec/cinch-convert_spec.rb
@@ -12,7 +12,7 @@ describe Cinch::Plugins::Convert do
     it 'should handle the units binary\'s absence with grace' do
       @bot = make_bot(Cinch::Plugins::Convert, { :units_path => '/usr/baddir/units' })
       msg = make_message(@bot, '!convert 3 foo to bar')
-      get_replies(msg).last.chomp.
+      get_replies(msg).last.text.
         should == 'test: Sorry, there\'s a configuration issue.'
     end
   end
@@ -20,25 +20,25 @@ describe Cinch::Plugins::Convert do
   describe 'conversions' do
     it 'should alow users to convert units' do
       msg = make_message(@bot, '!convert 2 inches to feet')
-      get_replies(msg).last.chomp.
+      get_replies(msg).last.text.
         should == 'test: 2 inches is 0.16666667 feet.'
     end
 
     it 'should allow users to convert temps' do
       msg = make_message(@bot, '!convert tempF(32) to tempC')
-      get_replies(msg).last.chomp.
+      get_replies(msg).last.text.
         should == 'test: tempF(32) is 0 tempC.'
     end
 
     it 'should return an error on conformability issues' do
       msg = make_message(@bot, '!convert 15 minutes to gallons')
-      get_replies(msg).last.chomp.
+      get_replies(msg).last.text.
         should == 'test: Sorry, there was a conformability error when making that conversion.'
     end
 
     it 'should return an error on invalid units' do
       msg = make_message(@bot, '!convert 15 foo to bar')
-      get_replies(msg).last.chomp.
+      get_replies(msg).last.text.
         should == 'test: Sorry, unknown unit \'foo\'.'
     end
 
